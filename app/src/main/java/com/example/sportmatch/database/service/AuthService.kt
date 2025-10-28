9b5ed110f5526a129f2af69305af875f54352eca
MarcosFelype <mfs22@aluno.ifal.edu.br>
2025-10-28 00:02:39 -0300

    feat: integration firebase auth - register

---
+++ app/src/main/java/com/example/sportmatch/database/service/AuthService.kt
@@ -0,0 +1,29 @@
+package com.example.sportmatch.database.service
+
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+
+
+class AuthService {
+
+    private val auth: FirebaseAuth = Firebase.auth
+
+    fun cadastrarUsuario(
+        email: String,
+        senha: String,
+        onComplete: (sucesso: Boolean, uid: String?, erroMsg: String?) -> Unit
+    ) {
+
+        auth.createUserWithEmailAndPassword(email, senha)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    val uid = task.result?.user?.uid
+                    onComplete(true, uid, null)
+                } else {
+                    val errorMessage = task.exception?.localizedMessage ?: "Erro desconhecido."
+                    onComplete(false, null, errorMessage)
+                }
+            }
+    }
+}
