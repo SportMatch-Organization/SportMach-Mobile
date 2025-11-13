a3c792b35544aa52682d861fdc7b41329db6c9dd
Calos Daniel <julio>
2025-11-13 00:19:00 -0300

    feat: adição da funcionalidade de cadastro de espaço esportivo

---
+++ app/src/main/java/com/example/sportmatch/ui/components/Loading.kt
@@ -0,0 +1,23 @@
+package com.example.sportmatch.ui.components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Loading(visible: Boolean) {
+    if (visible) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(color = MaterialTheme.colorScheme.primary)
+        }
+    }
+}
